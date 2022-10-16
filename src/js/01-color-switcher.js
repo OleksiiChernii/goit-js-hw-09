1e804112c66cc2fad8cfede0786c82a42647b477
OleksiiChernii <oleksii.chernii@gmail.com>
2022-10-16 19:56:12 +0300

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const btnRef = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+};
 
+btnRef.start.addEventListener('click', startGenerateColors);
+btnRef.stop.addEventListener('click', stopGeneratoColors);
+
+let randomId;
+
+function startGenerateColors(){
+    btnRef.start.disabled = !(btnRef.stop.disabled = false);
+    randomId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function stopGeneratoColors(){
+    btnRef.stop.disabled = !(btnRef.start.disabled = false);
+    clearInterval(randomId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
